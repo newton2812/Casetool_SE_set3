d73f586c628824ea9473a3d465913bef74ccca04
newton2812 <newtonvital@gmail.com>
2024-04-29 09:21:59 +0530

    initial commit

---
+++ Casetool_se_set3/src/book.java
@@ -0,0 +1,16 @@
+
+public class book {
+	
+	private String title;
+	private String author;
+	public book(String title, String author) {
+		this.title=title;
+		this.author=author;
+	}
+	
+	public String getTitle() {
+		return this.title;
+		
+	}
+
+}
